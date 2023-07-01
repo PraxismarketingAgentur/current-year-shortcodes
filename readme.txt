23281348cdad0ae2345778cf410a48701e333251
Praxismarketing Agentur <137172862+PraxismarketingAgentur@users.noreply.github.com>
2023-07-01 12:35:42 +0200

    Add files via upload

---
+++ readme.txt
@@ -0,0 +1,41 @@
+=== Current Year Shortcodes ===
+Contributors: praxismarketing
+Tags: shortcode, year, current, month, day, date, time
+Requires at least: 4.0
+Tested up to: 6.2.2
+Stable tag: 0.2
+License: GPLv2 or later
+License URI: https://www.gnu.org/licenses/gpl-2.0.html
+
+Insert the current year, month and day with a shortcode in any page, post or widget of your WordPress site.
+
+== Description ==
+
+**Current Year Shortcodes** lets you insert the current year, month and day anywhere in your WordPress site with a simple shortcode.
+
+Use the shortcode **[Y]** in any post, page or widget to display the current year.
+
+## How it works
+
+**[Y]** - inserts the current year with four digits e.g. **2023**
+**[y]** - inserts the current year with two digits e.g. **23**
+**[m]** - inserts the current month with two digits e.g. **01**
+**[n]** - inserts the current month without leading zeros e.g. **1**
+**[F]** - inserts the current month as a text e.g. **January**
+**[M]** - inserts the current month with three letters e.g. **Jan**
+**[d]** - inserts the current day with two digits e.g. **06**
+**[j]** - inserts the current day without leading zeros e.g. **6**
+**[D]** - inserts the current day with three letters e.g. **Mon**
+**[l]** - inserts the current day as a text e.g. **Monday**
+
+
+== Changelog ==
+
+= 0.2 =
+
+* Added current month and current day
+* Added logo
+
+= 0.1 =
+
+* Initial Release
---
+++ wp-year-shortcode.php
@@ -0,0 +1,66 @@
+<?php
+
+/**
+ * Plugin Name:       Current Year Shortcodes
+ * Plugin URI:        https://praxismarketing-agentur.com/wordpress-plugins/current-year-shortcodes/
+ * Description:       Add shortcodes for the current year, month and day
+ * Version:           0.2
+ * Author:            Lukas Pugstaller
+ * Author URI:        https://praxismarketing-agentur.com/
+ * License:           GPL v2 or later
+ * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
+ * Text Domain:       current-year-shortcodes
+ */
+
+// If accessed directly, exit
+if ( ! defined( 'ABSPATH' ) ) exit;
+
+function CurrentYearShortcodes_year_long() {
+    return date('Y');
+}
+add_shortcode( 'Y', 'CurrentYearShortcodes_year_long' );
+
+function CurrentYearShortcodes_year_short() {
+    return date('y');
+}
+add_shortcode( 'y', 'CurrentYearShortcodes_year_short' );
+
+function CurrentYearShortcodes_month_numeric_long() {
+    return date('m');
+}
+add_shortcode( 'm', 'CurrentYearShortcodes_month_numeric_long' );
+
+function CurrentYearShortcodes_month_numeric_short() {
+    return date('n');
+}
+add_shortcode( 'n', 'CurrentYearShortcodes_month_numeric_short' );
+
+function CurrentYearShortcodes_month_textual_long() {
+    return date('F');
+}
+add_shortcode( 'F', 'CurrentYearShortcodes_month_textual_long' );
+
+function CurrentYearShortcodes_month_textual_short() {
+    return date('M');
+}
+add_shortcode( 'M', 'CurrentYearShortcodes_month_textual_short' );
+
+function CurrentYearShortcodes_day_numeric_long() {
+    return date('d');
+}
+add_shortcode( 'd', 'CurrentYearShortcodes_day_numeric_long' );
+
+function CurrentYearShortcodes_day_numeric_short() {
+    return date('j');
+}
+add_shortcode( 'j', 'CurrentYearShortcodes_day_numeric_short' );
+
+function CurrentYearShortcodes_day_textual_long() {
+    return date('l');
+}
+add_shortcode( 'l', 'CurrentYearShortcodes_day_textual_long' );
+
+function CurrentYearShortcodes_day_textual_short() {
+    return date('D');
+}
+add_shortcode( 'D', 'CurrentYearShortcodes_day_textual_short' );
